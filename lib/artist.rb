0ba68c665d875e4954133dffef568e499f55b5e3
Santiago Sanchez <chagostwo@yahoo.com>
2019-06-13 02:56:07 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,6 +3,15 @@ class Artist
 
   def initialize(name)
     @name = name
+    @songs = []
   end
 
+  def add_song(song)
+    @songs << song
+    song.artist = self
+  end
+
+  def print_songs
+    @songs
+  end
 end
