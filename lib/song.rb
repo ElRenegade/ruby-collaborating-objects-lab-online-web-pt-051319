5d058fd7463c57ec00e458b34dbeb7e01b5b10d6
Santiago Sanchez <chagostwo@yahoo.com>
2019-06-13 03:50:01 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,25 @@
+class Song
+  attr_accessor :name, :artist
+  def initialize(name)
+    @name = name
+  end
+
+  def name=(name)
+    if self.name.nil?
+      self.name = Song.new(name)
+    else
+      slef.song.name = name
+  end
+
+  def artist=(name)
+    slef.artist = Artist.find_or_create_by_name(name)
+    artist.add_song(self)
+  end
+
+  def self.new_by_filename(file)
+    song = self.new
+    song.name = file.split(" - ")[1]
+    artist = file.split(" - ")[0]
+    song
+  end
+end
