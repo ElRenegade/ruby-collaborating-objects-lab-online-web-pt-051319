8288b59e2ea31ae6f3ec24c4947af4c109cceab1
Santiago Sanchez <chagostwo@yahoo.com>
2019-06-13 04:03:56 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,42 @@
+class Artist
+  attr_accessor :name, :songs
+
+  @@all = [] # array of artists instances
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def self.all
+    @@all
+  end
+
+  def add_song(song)
+    @songs << song
+  end
+
+  def self.find_or_create_by_name(name)
+    find(name) || create(name)
+  end
+
+  def self.find(name)
+    @@all.find do |artist|
+      artist.name == name
+    end
+  end
+
+  def self.create(name)
+    artist = new(name)
+    @@all << artist
+    artist
+  end
+
+  def save
+    @@all << self
+  end
+
+  def print_songs
+    self.songs.each {|song| puts song.name}
+  end
+end
