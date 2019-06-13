deb743029fb4d568843c03183b769e4a202c2d30
Santiago Sanchez <chagostwo@yahoo.com>
2019-06-13 04:00:02 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,25 +3,16 @@ class Artist
 
   @@all = [] # array of artists instances
 
-  def initialize(name) # artist_name
+  def initialize(name) 
     @name = name
-    @songs = [] # array of songs for each instance
+    @songs = [] 
   end
-
-  # this exposes the @@all Class Variable
+  
   def self.all
     @@all
   end
 
   def add_song(song)
-    # called by the song instance, adds in that song instance
-    # called inside the song method #artist_name=
-    # after the song has been created, assigned a name
-    # the song class checks to see if there is an artist associated with the song, and if there isnt' one, it asks the Artist class to create an artist instance
-    # it then passes that artist instance back to the song to store as an attribute
-    # the song class then calls this method #add_song
-    # this method adds the song to the artist instances song array
-    # every artist instance has this array
     @songs << song
   end
 
@@ -45,8 +36,6 @@ class Artist
     @@all << self
   end
 
-  # this method prints out an array of song names
-  # by accessing the name attribute of each song
   def print_songs
     puts @songs.collect(&:name)
   end
